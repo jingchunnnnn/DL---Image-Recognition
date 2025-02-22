c87da9154a65821a79f8c71409556f5265375811
Jing Chun <jakezhou2004@gmail.com>
2025-02-23 00:57:33 +0800

    Added link to kaggle dataset

--- imgrecognition.py
+++ imgrecognition.py
@@ -1,3 +1,5 @@
+# Link to dataset: https://www.kaggle.com/datasets/mdwaquarazam/agricultural-crops-image-classification
+
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
